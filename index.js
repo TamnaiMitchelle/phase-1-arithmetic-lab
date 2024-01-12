76313b9daf0404d286c920db5c76a6e3047ff96f
Tamnai Mitchelle <mitchellecheptoek@gmail.com>
2024-01-12 22:36:59 +0300

    Modified file

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random() * 10)
+
+const num3 = 5
+const num4 = 34
+let mod = num4 % num3
+
+let max = Math.max(16, 13, 20, 5, 2)
